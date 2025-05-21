d01caeff8ac43238fdb2abc77ba5757b48248d66
Mohd-ayank <ayanmdkhan2340@gmail.com>
2025-05-21 23:00:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login at chai or code</h1>')
+}
+)
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
