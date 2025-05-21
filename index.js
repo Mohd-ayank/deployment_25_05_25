4f77f5c093c94e12913c4d17749fefc57fb4aade
Mohd-ayank <ayanmdkhan2340@gmail.com>
2025-05-21 23:23:47 +0530

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+const githubData={
+    "id":"kmqo1ybduigr3dugd",
+    "email":"anid@dknifnkd.com",
+    "name":"ksknosidio"
+}
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -10,6 +15,10 @@ app.get('/login',(req,res)=>{
     res.send('<h1>please login at chai or code</h1>')
 }
 )
+
+app.get('/github',(req,res)=>{
+    res.json(githubData);
+})
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
 })
